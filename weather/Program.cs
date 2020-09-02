5d5bfa458c6475427576941eaf3647184d75adae
Vladimir-Molchamov <vovinator03@gmail.com>
2020-09-02 20:58:30 +0600

    wet1

---
+++ weather/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weather;
+            int celsius;
+            Random rand = new Random();
+            weather = rand.Next(1, 4);
+            celsius = rand.Next(-40, 40);
+            switch (weather)
+            {
+                case 1:
+                    Console.WriteLine("Ясная");
+                    Console.WriteLine(celsius);
+                    break;
+                case 2:
+                    Console.WriteLine("дождь");
+                    Console.WriteLine(celsius);
+                    break;
+                case 3:
+                    Console.WriteLine("облачно");
+                    Console.WriteLine(celsius);
+                    break;
+                case 4:
+                    Console.WriteLine("снег");
+                    Console.WriteLine(celsius);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
